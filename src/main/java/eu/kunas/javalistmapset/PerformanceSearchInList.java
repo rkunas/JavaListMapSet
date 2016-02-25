584bfc992d9635e2e6ce03de2658b2ffaa3b8a1f
Ramazan Kunas <ramazan@kunas.eu>
2016-02-25 10:15:18 +0100

    Removed comment

--- src/main/java/eu/kunas/javalistmapset/PerformanceSearchInList.java
+++ src/main/java/eu/kunas/javalistmapset/PerformanceSearchInList.java
@@ -6,7 +6,7 @@ import eu.kunas.javalistmapset.performances.LambdaListSearchPerformance;
  * Created by RKU on 23.02.2016.
  */
 public class PerformanceSearchInList {
-                                //
+
     private static final Integer times = 10000000;
 
     public static void main(String[] args) {
